5bccfdbc57118731b1c554d55d699db20fe1c677
h15S03y91 <harsinyee315@gmail.com>
2022-12-14 09:32:06 +0800

    refactor builder progress to animation

--- example/lib/notification/custom_animation.dart
+++ example/lib/notification/custom_animation.dart
@@ -3,23 +3,31 @@ import 'package:overlay_support_example/notification/ios_toast.dart';
 
 /// Example to show how to popup overlay with custom animation.
 class CustomAnimationToast extends StatelessWidget {
-  final double value;
+  final Animation<double> animation;
 
   static final Tween<Offset> tweenOffset =
       Tween<Offset>(begin: Offset(0, 40), end: Offset(0, 0));
 
   static final Tween<double> tweenOpacity = Tween<double>(begin: 0, end: 1);
 
-  const CustomAnimationToast({Key? key, required this.value}) : super(key: key);
+  const CustomAnimationToast({Key? key, required this.animation})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Transform.translate(
-      offset: tweenOffset.transform(value),
-      child: Opacity(
-        opacity: tweenOpacity.transform(value),
-        child: IosStyleToast(),
-      ),
+    final animation =
+        CurveTween(curve: Curves.decelerate).animate(this.animation);
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: tweenOffset.transform(animation.value),
+          child: FadeTransition(
+            opacity: animation,
+            child: IosStyleToast(),
+          ),
+        );
+      },
     );
   }
 }
